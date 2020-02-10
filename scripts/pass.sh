0a7c6e347b86e98199b46446bb347dd271bfa0be
Tiago Baeta <tnbaeta@gmail.com>
2020-02-10 14:34:19 -0300

    version 1.0 of pass.sh script tested

--- scripts/pass.sh
+++ scripts/pass.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# arguments.sh
+# pass.sh
 
-# Default values of arguments
+# Variables to hold the arguments to pass
 PASSWORD=""
 FILE=""
 IP_ADDR=""
@@ -14,7 +14,7 @@ do
     case $arg in
         -p|--password)
         PASSWORD="$2"
-        shift # Remove --initialize from processing
+        shift
         ;;
         -f|--file)
 				FILE="$3"
@@ -35,26 +35,34 @@ do
     esac
 done
 
+# Functions:
+
+# Creates a local file to store the password (clear text)
 createFile() {
 	echo "$PASSWORD" > "$FILE"
 }
 
+# Generate the private key
 genKey() {
 	ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa <<< y
 }
 
+# Add the remote host to the list of known hosts for SSH operations
 keyScan() {
 	ssh-keyscan -H "$IP_ADDR" >> ~/.ssh/known_hosts
 }
 
+# Copy the public key to remote host for SSH operations using 'sshpass' with provided password
 keyCopy() {
 	sshpass -f "$FILE" ssh-copy-id "$IP_ADDR"
 }
 
+# Copy the artifacts to Tomcat web app home
 copyArtifact() {
 	scp "$ARTIFACT" "$IP_ADDR":"$CATALINA_HOME"/webapps
 }
 
+# Call all the functions
 createFile
 genKey
 keyScan
